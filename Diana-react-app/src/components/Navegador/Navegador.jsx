a1cc8cbf986b8bf48b86b434086c27886e8b334b
unknown <villalv.diana7368@gmail.com>
2024-10-27 05:03:48 -0500

    Avance Navegador parte 1

--- Diana-react-app/src/components/Navegador/Navegador.jsx
+++ Diana-react-app/src/components/Navegador/Navegador.jsx
@@ -1,50 +1,22 @@
 import React, { useState } from 'react';
 import './Navegador.css';
-import Logo from '../../assets/jordan.svg';
+import Logo from '../../assets/logo_nikes.png';
 
-const Header = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const handleMouseEnter = () => {
-    setIsOpen(true);
-  };
-
-  const handleMouseLeave = () => {
-    setIsOpen(false);
-  };
+const Navegador = () => {
 
   return (
-    <header>
-      <img src={Logo} alt="Logo Jordan" className="imagen_header" />
-      <ul>
-        <li><a href="#">Buscar&nbsp;tienda</a></li>
-        <li className="divisionHeader">|</li>
-
-        <li 
-          className="dropdown-container" 
-          onMouseEnter={handleMouseEnter} 
-          onMouseLeave={handleMouseLeave}
-        >
-          <a href="#" >Ayuda</a>
-          {isOpen && (
-            <ul className="dropdown-menu">
-              <li><a href="#">Estado del pedido</a></li>
-              <li><a href="#">Políticas de envío</a></li>
-              <li><a href="#">Devoluciones</a></li>
-              <li><a href="#">Comunícate con nosotros</a></li>
-              <li><a href="#">Política de privacidad</a></li>
-              <li><a href="#">Términos y Condiciones</a></li>
-            </ul>
-          )}
-        </li>
-
-        <li className="divisionHeader">|</li>
-        <li><a href="#">Únete</a></li>
-        <li className="divisionHeader">|</li>
-        <li><a href="#">Iniciar sesión</a></li>
-      </ul>
-    </header>
+    <nav>
+        <img src={Logo} alt="Logo Nike" className='imagen_navegador'/>
+        <ul>
+          <li><a href="#">Lo nuevo</a></li>
+          <li><a href="#">Hombre</a></li>
+          <li><a href="#">Mujer</a></li>
+          <li><a href="#">Niños</a></li>
+          <li><a href="#">Accesorios</a></li>
+          <li><a href="#">Descuentos</a></li>
+        </ul>
+    </nav>
   );
 };
 
-export default Header;
+export default Navegador;
--- Diana-react-app/src/main.jsx
+++ Diana-react-app/src/main.jsx
@@ -4,9 +4,12 @@ import './index.css'
 import App from './App.jsx'
 // import Header from './components/Navegador/Navegador.jsx'
 import Header from'./components/Header/Header.jsx'
+import Navegador from'./components/Navegador/Navegador.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header/>
+    <Navegador/>
   </StrictMode>,
 )
