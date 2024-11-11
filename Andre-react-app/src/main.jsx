17bb03cd3c0029acd659e50231167880eca48d43
RoberGonas2003 <20211165@aloe.ulima.edu.pe>
2024-11-10 21:58:56 -0500

    Subiendo mi parte

--- Andre-react-app/src/main.jsx
+++ Andre-react-app/src/main.jsx
@@ -1,9 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import Header from '../../Diana-react-app/src/components/Header/Header.jsx'
-import Navegador from '../../Diana-react-app/src/components/Navegador/Navegador.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import Header from '../../Diana-react-app/src/components/Header/Header.jsx';
+import Navegador from '../../Diana-react-app/src/components/Navegador/Navegador.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
