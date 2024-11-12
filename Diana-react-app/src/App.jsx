e9b9d44d3b7fb89dc37df1827e483482c8ee9c1a
unknown <villalv.diana7368@gmail.com>
2024-11-12 02:30:08 -0500

    carrusel 3,4,5 y 6

--- Diana-react-app/src/App.jsx
+++ Diana-react-app/src/App.jsx
@@ -27,13 +27,35 @@ const productos_crs2 = [
 ];
 
 const productos_crs3 = [
-  { image: '../../../../public/carrusel2/producto1_carrusel2.png', title: 'Nike Pegasus 41', description: '',price:'' },
-  { image: '../../../../public/carrusel2/producto2_carrusel2.png', title: 'Prepárate para el verano', description: '',price:'' },
-  { image: '../../../../public/carrusel2/producto3_carrusel2.png', title: 'Explora lo último de Jordan', description: '',price:'' },
-  { image: '../../../../public/carrusel2/producto4_carrusel2.png', title: 'Lo mejor de Nike Fútbol', description: '',price:'' },
-  { image: '../../../../public/carrusel2/producto4_carrusel2.png', title: 'Lo nuevo de Air Max', description: '',price:'' }
+  { image: '../../../../public/carrusel3/producto1_carrusel3.png', title: 'Air Force 1', description: '',price:'' },
+  { image: '../../../../public/carrusel3/producto2_carrusel3.png', title: 'Air Jordan', description: '',price:'' },
+  { image: '../../../../public/carrusel3/producto3_carrusel3.png', title: 'Air Max', description: '',price:'' },
+  { image: '../../../../public/carrusel3/producto4_carrusel3.png', title: 'Dunk', description: '',price:'' },
+  { image: '../../../../public/carrusel3/producto5_carrusel3.png', title: 'Blazer', description: '',price:'' },
+  { image: '../../../../public/carrusel3/producto6_carrusel3.png', title: 'Pegasus', description: '',price:'' }
 ];
 
+const productos_crs4 = [
+  { image: '../../../../public/carrusel4/producto1_carrusel4.jpg', title: '', description: '',price:'' },
+  { image: '../../../../public/carrusel4/producto2_carrusel4.jpg', title: '', description: '',price:'' },
+  { image: '../../../../public/carrusel4/producto3_carrusel4.png', title: '', description: '',price:'' }
+  ];
+const productos_crs5 = [
+  { image: '../../../../public/carrusel5/producto1_carrusel5.png', title: 'Training', description: '',price:'' },
+  { image: '../../../../public/carrusel5/producto2_carrusel5.png', title: 'Fútbol', description: '',price:'' },
+  { image: '../../../../public/carrusel5/producto3_carrusel5.png', title: 'Tenis', description: '',price:'' },
+  { image: '../../../../public/carrusel5/producto4_carrusel5.png', title: 'Skate', description: '',price:'' },
+  { image: '../../../../public/carrusel5/producto5_carrusel5.png', title: 'Running', description: '',price:'' },
+  { image: '../../../../public/carrusel5/producto6_carrusel5.png', title: 'Yoga', description: '',price:'' }
+];
+
+const productos_crs6 = [
+  { image: '../../../../public/carrusel6/producto1_carrusel6.png', title: 'Training', description: '',price:'' },
+  { image: '../../../../public/carrusel6/producto2_carrusel6.png', title: 'Fútbol', description: '',price:'' },
+  { image: '../../../../public/carrusel6/producto3_carrusel6.png', title: 'Tenis', description: '',price:'' },
+  { image: '../../../../public/carrusel6/producto4_carrusel6.png', title: 'Skate', description: '',price:'' },
+  { image: '../../../../public/carrusel6/producto5_carrusel6.png', title: 'Running', description: '',price:'' }
+];
   return (
     <div>
      <Header/>
@@ -41,7 +63,7 @@ const productos_crs3 = [
     <PromoHeader/> 
     <div className='divisionprincipal'>
       <section>
-        <article>
+        <article className='artic'>
           <h3>¡Los favoritos del momento! ⚡ ¿Ya tienes el tuyo?</h3>
           <button className='bontones_sec1'></button>
           <button className='bontones_sec1_2'></button>
@@ -57,7 +79,7 @@ const productos_crs3 = [
             />
           </div>
 
-        <article>
+        <article className='artic'>
         <h3>Novedades</h3>
           <button className='bontones_sec1'></button>
           <button className='bontones_sec1_2'></button>
@@ -73,26 +95,65 @@ const productos_crs3 = [
                 
             />
           </div>
-        <article>
+        <article className='artic'>
         <h3>Siempre Icónico</h3>
           <button className='bontones_sec2'></button>
           <button className='bontones_sec2_2'></button>
         </article>
-
-        <article>
+        <div className='carrusel3'> 
+          <Carrusel
+                items={productos_crs3}
+                imageWidth="250px"
+                imageHeight="300px"
+                fontSize="16px"
+                textAlignment="left"
+                titleFontSize="16px"
+                
+            />
+          </div>
+        <article className='artic'>
         <h3>Comprar por género</h3>
-          <button disabled className='bontones_sec1'></button>
-          <button className='bontones_sec1_2'></button>
+          <button className='bontones_sec1' disabled></button>
+          <button className='bontones_sec1_2' disabled></button>
         </article>
-        
-        <article>
+        <div className='carrusel4'> 
+          <Carrusel
+                items={productos_crs4}
+                imageWidth="465px"
+                imageHeight="580px"
+                fontSize="16px"
+                textAlignment="left"
+                titleFontSize="16px"
+                
+            />
+          </div>
+        <article className='artic'>
           <button className='bontones_sec2'></button>
           <button className='bontones_sec2_2'></button>
         </article>
-        
-        <article>
+        <div className='carrusel5'> 
+          <Carrusel
+                items={productos_crs5}
+                imageWidth="250px"
+                imageHeight="172px"
+                fontSize="16px"
+                textAlignment="left"
+                TitlefontSize="20px"
+            />
+          </div>
+        <article className='artic'>
           <h3>Nike Membership</h3>
         </article>
+        <div className='carrusel6'> 
+          <Carrusel
+                items={productos_crs6}
+                imageWidth="252px"
+                imageHeight="260px"
+                fontSize="16px"
+                textAlignment="left"
+                TitlefontSize="20px"
+            />
+          </div>
       </section>
       </div>
     </div>
